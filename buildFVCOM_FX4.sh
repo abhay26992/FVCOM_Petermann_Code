f2839f92a0187da8039b3683aee12ae44da18a93
Rupert Gladstone <gladston@puhti-login2.bullx>
2019-10-08 16:48:29 +0300

    initial changes to set up FVCOM to run with FISOC on puhti.csc.fi

---
+++ buildFVCOM_FX4.sh
@@ -0,0 +1,14 @@
+
+cd /projappl/project_2000339/source/esmf-fvcom-fisoc/FVCOM_source
+cp make.inc.puhti make.inc
+#export FC=mpiifort
+#export CFLAGS=" -O3 -fPIC"
+#export OPT=-O3
+#export CPP= /usr/bin/cpp
+#export COMPILER= -DMPIIFORT  
+cp /projappl/project_2000339/source/esmf-fvcom-fisoc/FVCOM_source/libfvcom_api.so /projappl/project_2000339/installs/FVCOM_FX4/
+cp /projappl/project_2000339/source/esmf-fvcom-fisoc/FVCOM_source/fvcom_driver /projappl/project_2000339/installs/FVCOM_FX4/
+
+make clean
+make
+
