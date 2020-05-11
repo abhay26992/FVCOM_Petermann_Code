8b2e3ebcef6e2e93084ebf0557832d39005ca723
Rupert Gladstone <rupertgladstone1972@gmail.com>
2020-05-11 16:21:36 +0300

    minor changes to allow FVCOM ISOMIP build

---
+++ buildFVCOM_MM.sh
@@ -0,0 +1,21 @@
+
+module purge
+
+module load intel/19.0.4 intel-mpi/18.0.5 intel-mkl/2019.0.4
+module load esmf
+module load elmer/fisoc
+module load fvcom/FX4
+module load cmake
+module load netcdf/4.7.0
+module load netcdf-fortran/4.4.4
+module list
+
+python buildFVCOM_MM.py
+
+#export FC=mpiifort
+#export CFLAGS=" -O3 -fPIC"
+#export OPT=-O3
+#export CPP= /usr/bin/cpp
+#export COMPILER= -DMPIIFORT  
+
+
